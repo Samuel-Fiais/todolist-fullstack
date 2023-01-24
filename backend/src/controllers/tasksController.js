e8ee38d1033968ed2975af399aeaa3102a3201ed
Samuel Fiais <80596893+Samuel-Fiais@users.noreply.github.com>
2023-01-23 23:11:53 -0300

    Update tasksController.js

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,6 +1,6 @@
 const taskModels = require('../models/tasksModel')
 
-const getAll = async (req, res) => {
+const getAll = async (_req, res) => {
 	try {
 		const tasks = await taskModels.getAll()
 		return res.status(200).json(tasks)
@@ -11,6 +11,22 @@ const getAll = async (req, res) => {
 	}
 }
 
+const createTask = async (req, res) => {
+	try {
+		const createdTask = await taskModels.createTask(req.body)
+		return res.status(201).json({
+			message: 'Created ' + req.body.title,
+			insertId: createdTask
+		})
+		
+	} catch (err) {
+		return res.status(401).json({
+			message: err.message
+		})
+	}
+}
+
 module.exports = {
-	getAll
+	getAll,
+	createTask
 }
