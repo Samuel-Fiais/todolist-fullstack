b8df8315f935aa145ed58966244e0753e9f050e1
Samuel Fiais <80596893+Samuel-Fiais@users.noreply.github.com>
2023-01-23 21:49:30 -0300

    Add Query Get All Tasks

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -1,5 +1,8 @@
-const getAll = () => {
+const connection = require('./connection')
 
+const getAll = async () => {
+	const tasks = await connection.execute('SELECT * FROM TODO.TASKS')
+	return tasks
 }
 
 module.exports = {
