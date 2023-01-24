7bf34198ec3211d554c3e91297d41a15f8d470f4
Samuel Fiais <80596893+Samuel-Fiais@users.noreply.github.com>
2023-01-23 21:40:26 -0300

    Update Router.js and Server.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
+// Router Get Test
 router.get('/', (req, res) => {
 	res.status(200).send('Welcome to the TodoList')
 })
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,7 @@
 const app = require('./app')
+require('dotenv').config()
 
-app.listen(3333, () => console.log('Server running on port 3333'))
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
